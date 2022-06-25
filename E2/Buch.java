7f863f8fbfc5d7c8a0820067b93a6ce100f44351
Mouayad <84500993+Mouayad91@users.noreply.github.com>
2022-06-26 01:14:44 +0200

    Add files via upload

---
+++ E2/Buch.java
@@ -0,0 +1,56 @@
+public class Buch extends Artikel {
+
+	private String autor;
+	private String titel;
+	private String verlag;
+
+	
+	public Buch(int artikelNr, String art, double preis,String autor, String titel, String verlag) {
+		super(artikelNr, art, preis);
+	}
+
+
+	public String getAutor() {
+		return autor;
+	}
+
+
+	public void setAutor(String autor) {
+		
+		  if (autor != null) {
+	          this.setAutor(autor);
+	      } else {
+	          throw new IllegalArgumentException("Interpret muss angegeben werden");
+	      }
+
+	}
+
+
+	public String getTitel() {
+		return titel;
+	}
+
+
+	public void setTitel(String titel) {
+		  if (titel != null) {
+	          this.setTitel(titel);
+	      } else {
+	          throw new IllegalArgumentException("titel muss angegeben werden");
+	      }
+
+	}
+
+
+	public String getVerlag() {
+		return verlag;
+	}
+
+
+	public void setVerlag(String verlag) {
+		  if (verlag != null) {
+	          this.setVerlag(verlag);
+	      } else {
+	          throw new IllegalArgumentException("verlag muss angegeben werden");
+	      }
+
+	}}
